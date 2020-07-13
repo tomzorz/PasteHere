f1a818862821688cb5f2967d03aa610609f8d6bc
Tamás Deme <tomzorz@outlook.com>
2020-07-13 02:11:21 +0200

    initial commit 2: the initialization continues

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PasteHere
 A tool that helps you paste an image or text from your clipboard as a file
+
+[demo gif](https://raw.githubusercontent.com/tomzorz/PasteHere/master/demo.gif)
+
+Built using .net core, a few WinForms APIs and an extra sprinkle of p/invoke shell commands. 
+
+## Usage
+
+1. Unpack the release somewhere
+2. Edit the .reg file using your editor of choice, and change the binary path to match wherever you extracted it
+3. Double-click the .reg file to add it to your registry
+4. If you did everything well, right clicking an empty space inside the folder should have the new option
+
+Right now the tool outputs .txt and .png files for text and image content respectively.
---
+++ sources/PasteHere/PasteHere/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PasteHere
+{
+    class Program
+    {
+        // thanks to https://superuser.com/questions/445925/how-to-add-item-to-right-click-menu-when-not-selecting-a-folder-or-file for the regedit
+
+        [STAThread()]
+        static void Main(string[] args)
+        {
+            var fp = Path.Combine(Environment.CurrentDirectory, $"_PH_{Guid.NewGuid()}");
+
+            if (Clipboard.ContainsImage())
+            {
+                fp += ".png";
+                var image = Clipboard.GetImage();
+                using var fs = File.OpenWrite(fp);
+                Debug.Assert(image != null, nameof(image) + " != null");
+                image.Save(fs, ImageFormat.Png);
+
+            } else if (Clipboard.ContainsText())
+            {
+                fp += ".txt";
+                var text = Clipboard.GetText();
+                File.WriteAllText(fp, text);
+            }
+
+            SelectItemInExplorer(fp, true);
+        }
+
+        // thanks to https://stackoverflow.com/questions/8647447/send-folder-rename-command-to-windows-explorer
+        // and https://stackoverflow.com/questions/3010305/programmatically-selecting-file-in-explorer
+
+        public static void SelectItemInExplorer(string itemPath, bool edit)
+        {
+            if (itemPath == null)
+                throw new ArgumentNullException("itemPath");
+
+            var pidl = ILCreateFromPathW(itemPath);
+            IntPtr folder = PathToAbsolutePIDL(pidl, Path.GetDirectoryName(itemPath));
+            IntPtr file = PathToAbsolutePIDL(pidl, itemPath);
+            try
+            {
+                SHOpenFolderAndSelectItems(folder, 1, new[] { file }, edit ? 1 : 0);
+            }
+            finally
+            {
+                ILFree(folder);
+                ILFree(file);
+            }
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr ILCreateFromPathW(string pszPath);
+
+        [DllImport("shell32.dll")]
+        private static extern int SHOpenFolderAndSelectItems(IntPtr pidlFolder, uint cidl, IntPtr[] apidl, int dwFlags);
+
+        [DllImport("shell32.dll")]
+        private static extern void ILFree(IntPtr pidl);
+
+        [DllImport("shell32.dll")]
+        private static extern int SHGetDesktopFolder(out IShellFolder ppshf);
+
+        [DllImport("ole32.dll")]
+        private static extern int CreateBindCtx(int reserved, out IBindCtx ppbc);
+
+        [ComImport, Guid("000214E6-0000-0000-C000-000000000046"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IShellFolder
+        {
+            void ParseDisplayName(IntPtr hwnd, IBindCtx pbc, [In, MarshalAs(UnmanagedType.LPWStr)] string pszDisplayName, out uint pchEaten, out IntPtr ppidl, ref uint pdwAttributes);
+            // NOTE: we declared only what we needed...
+        }
+
+        private static IntPtr GetShellFolderChildrenRelativePIDL(IntPtr hwnd, IShellFolder parentFolder, string displayName)
+        {
+            IBindCtx bindCtx;
+            CreateBindCtx(0, out bindCtx);
+            uint pchEaten;
+            uint pdwAttributes = 0;
+            IntPtr ppidl;
+            parentFolder.ParseDisplayName(hwnd, bindCtx, displayName, out pchEaten, out ppidl, ref pdwAttributes);
+            return ppidl;
+        }
+
+        private static IntPtr PathToAbsolutePIDL(IntPtr hwnd, string path)
+        {
+            IShellFolder desktopFolder;
+            SHGetDesktopFolder(out desktopFolder);
+            return GetShellFolderChildrenRelativePIDL(hwnd, desktopFolder, path);
+        }
+    }
+}
